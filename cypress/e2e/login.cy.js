f1844f61d80399b7d062450cfa1d6b7ad4017506
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2023-06-20 15:56:15 +0200

    randomize user name

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,22 @@
-describe('Przykładowy test Cypress', () => {
-  it('Logowanie na stronie Sauce Demo', () => {
-    cy.visit('/', { retryOnStatusCodeFailure: true, retryOnNetworkFailure: true, retry: 2 });
-    cy.get('#signin2').click();
-    cy.get('#sign-username').type(Cypress.env('user_name')) //metody do randomowego tworzenia usera
-    cy.get('#sign-password').click().type(Cypress.env('password')) //metoda do tworzenia hasla
-    cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+import faker from "faker";
+
+describe("Przykładowy test Cypress", () => {
+  it("Logowanie na stronie Sauce Demo", () => {
+    const randomUsername = faker.internet.userName();
+    cy.visit("/", {
+      retryOnStatusCodeFailure: true,
+      retryOnNetworkFailure: true,
+      retry: 2,
+    });
+    cy.get("#signin2").click();
+    cy.get("#sign-username").type(randomUsername);
+    cy.get("#sign-password").click().type(Cypress.env("password"), {log:false});
+    cy.get(
+      "#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary"
+    ).click();
+
+    cy.on("window:alert", (text) => {
+      expect(text).to.eql('Sign up successful.');
+    });
   });
 });
