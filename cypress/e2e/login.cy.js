2fb3ad95a7b7b290f92cddc11e1810c9eb51c565
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2023-06-22 15:41:56 +0200

    dodanie zapasowego auth

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,9 +8,12 @@ describe("Logowanie do sklepu", () => {
   });
 
   it("Wprowadzenie danych do logowania", () => {
+    const user = Cypress.env("LOGIN") || Cypress.env("user");
+    const password = Cypress.env("PASSWORD") || Cypress.env("password");
+
     cy.get("#login2").click();
-    cy.get("#loginusername").type(Cypress.env("LOGIN"), { delay: 10 });
-    cy.get("#loginpassword").click().type(Cypress.env("PASSWORD"), { log: false });
+    cy.get("#loginusername").type(user, { delay: 10 });
+    cy.get("#loginpassword").click().type(password, { log: false });
     cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
 
     cy.on("window:alert", (text) => {
