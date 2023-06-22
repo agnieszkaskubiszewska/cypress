3713872941e1f27df40e3931c946de578609c909
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2023-06-22 15:31:02 +0200

    echo secretow

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,16 @@
-describe("Przykładowy test Cypress", () => {
-  it("Logowanie na stronie sklepu", () => {
-
+describe("Logowanie do sklepu", () => {
+  beforeEach(() => {
     cy.visit("/", {
       retryOnStatusCodeFailure: true,
       retryOnNetworkFailure: true,
       retry: 2,
     });
+  });
+
+  it("Wprowadzenie danych do logowania", () => {
     cy.get("#login2").click();
-    cy.get("#loginusername").type(Cypress.env("user"));
-    cy.get("#loginpassword").click().type(Cypress.env("password"), {log:false});
+    cy.get("#loginusername").type(Cypress.env("user"), { delay: 10 });
+    cy.get("#loginpassword").click().type(Cypress.env("password"), { log: false });
     cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
 
     cy.on("window:alert", (text) => {
@@ -16,3 +18,4 @@ describe("Przykładowy test Cypress", () => {
     });
   });
 });
+
