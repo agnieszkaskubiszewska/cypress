ac5f4a3ffef398c2ab06ad4b6f593135607bb9ae
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2023-07-15 11:46:43 +0200

    home page test added

--- cypress/pageObjects/homePage.js
+++ cypress/pageObjects/homePage.js
@@ -1,26 +1,37 @@
 class HomePage {
+  verifyLoggedInUser(expectedUsername) {
+    cy.get("#nameofuser").should("have.text", expectedUsername);
+  }
 
-    verifyLoggedInUser(expectedUsername) {
-        cy.get("#nameofuser").should("have.text", expectedUsername);
-      }
+  clickListItemByText(text) {
+    cy.get(".list-group-item").contains(text).click();
+  }
 
-      clickListItemByText(text) {
-        cy.get('.list-group-item').contains(text).click();
-      }
+  checkCarousel() {
+    const carousel = cy.get(".carousel-item").should("be.visible");
+  }
 
-      checkCarouselItemsActive() {
-        cy.get('.carousel-item').should(($items) => {
-          expect($items.some('.active')).to.be.true;
-        });
-      }
+  checkMenuItemt(text) {
+    cy.get(".navbar-example").should("not.be.empty");
+  }
 
-      clickMenuItemByText(text) {
-        cy.get('.navbar-example').should('not.be.empty').contains(text).click();
-      }
-     pageContent(){
-        cy.get('.tbodyid').should('not.be.empty')
-     }
-     bottomSection(){
-
-     }
+  clickOnCategory(category) {
+    switch (category) {
+      case "notebook":
+        cy.get("[onclick=\"byCat('notebook')\"]").click();
+        break;
+      case "monitor":
+        cy.get("[onclick=\"byCat('monitor')\"]").click();
+        break;
+      case "phone":
+        cy.get("[onclick=\"byCat('phone')\"]").click();
+        break;
+      default:
+        throw new Error("Nieznana kategoria: " + category);
+    }
+  }
+  pageContent() {
+    cy.get(".tbodyid").should("not.be.empty");
+  }
 }
+export default HomePage;
